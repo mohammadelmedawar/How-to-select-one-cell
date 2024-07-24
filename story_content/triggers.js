fedfdb5f9b7502a3e688179c48cd566bb834e7a7
Mohamed <mohammad.elmedawar@gmail.com>
2024-07-24 14:28:28 +0300

    Simulation

---
+++ story_content/triggers.js
@@ -0,0 +1,42 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6cgNHI8hPpb":
+        Script1();
+        break;
+      case "5nZJgNNribH":
+        Script2();
+        break;
+      case "6puVvQTBPtz":
+        Script3();
+        break;
+      case "6KyhHn7s2Ok":
+        Script4();
+        break;
+      case "6iDCXqsHm5R":
+        Script5();
+        break;
+      case "6AVfkxCh6ma":
+        Script6();
+        break;
+      case "65L6deUyNvr":
+        Script7();
+        break;
+      case "62AAuOIncnF":
+        Script8();
+        break;
+      case "5jbUqv3MCA8":
+        Script9();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+};
